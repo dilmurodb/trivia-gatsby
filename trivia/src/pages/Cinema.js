4c1f312aae9ebd1b21546f92bbfc9abd97d5c165
bukharov <dilmurod.bukharov@gmail.com>
2020-03-23 18:27:24 -0400

    created gatsby project and made small changes to make sure it works

---
+++ trivia/src/pages/Cinema.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const CinemaPage = () => {
+  return (
+    <div>
+      <h1>
+        Welcome to Cinema Category, please click the button below to start the
+        game
+      </h1>
+    </div>
+  )
+}
+
+export default CinemaPage
